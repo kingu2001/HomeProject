d9eef2f80601c168641d73814d1c9e796ab521f2
Kingu20 <kiettn12@gmail.com>
2023-07-06 14:57:35 +0200

    User class, UserVM, LoginWindow

---
+++ ATM Software/ATM Software/MVVM/Model/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_Software.MVVM.Model
+{
+    class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CreditCardNumber { get; set; }
+        public int PinCode { get; set; }
+
+        public User(string firstName, string lastName, string creditCardNumber, int pinCode)
+        {
+            firstName = FirstName; 
+            lastName = LastName;
+            CreditCardNumber = creditCardNumber;
+            PinCode = pinCode;
+        }
+    }
+}
---
+++ ATM Software/ATM Software/MVVM/ViewModel/UserVM.cs
@@ -0,0 +1,25 @@
+﻿using ATM_Software.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_Software.MVVM.ViewModel
+{
+    class UserVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CreditCardNumber { get; set; }
+        public int PinCode { get; set; }
+
+        public UserVM(User user)
+        {
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            CreditCardNumber = user.CreditCardNumber;
+            PinCode = user.PinCode;
+        }
+    }
+}
