77bda97e8258e1121b4927660d3dd79b584ab956
Kiet Nguyen <kietn72@gmail.com>
2023-09-25 13:03:27 +0200

    update

--- WeatherAPI/Controllers/WeatherForecastController.cs
+++ WeatherAPI/Controllers/WeatherForecastController.cs
@@ -19,12 +19,33 @@ namespace WeatherAPI.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get(int daysChosen)
         {
+            string url = "http://api.openweathermap.org/data/2.5/forecast?lat=51.5156177&lon=-0.0919983&units&appid=7bf36c7d7f2a40883ce3a0a41b19bd5a";
+            if (daysChosen == 1)
+            {
+                url = $"http://api.openweathermap.org/data/2.5/forecast?lat=51.5156177&lon=-0.0919983&units&cnt={daysChosen + 7}appid=7bf36c7d7f2a40883ce3a0a41b19bd5a";
+            }
+            else if (daysChosen == 2)
+            {
+                url = $"http://api.openweathermap.org/data/2.5/forecast?lat=51.5156177&lon=-0.0919983&units&cnt={daysChosen + 14}appid=7bf36c7d7f2a40883ce3a0a41b19bd5a";
+            }
+            else if (daysChosen == 3)
+            {
+                url = $"http://api.openweathermap.org/data/2.5/forecast?lat=51.5156177&lon=-0.0919983&units&cnt={daysChosen + 21}appid=7bf36c7d7f2a40883ce3a0a41b19bd5a";
+            }
+            else if (daysChosen == 4)
+            {
+                url = $"http://api.openweathermap.org/data/2.5/forecast?lat=51.5156177&lon=-0.0919983&units&cnt={daysChosen + 28}appid=7bf36c7d7f2a40883ce3a0a41b19bd5a";
+            }
+            if (daysChosen == 5)
+            {
+
+            }
             try
             {
                 var httpClient = new HttpClient();
-                var response = await httpClient.GetAsync("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=fc4747c60f1299d385354a09ad74e412");
+                var response = await httpClient.GetAsync(url);
                 response.EnsureSuccessStatusCode(); 
                 var data = await response.Content.ReadAsStringAsync();
                 return Content(data,"Application/json");
--- WeatherForecast/Controllers/HomeController.cs
+++ WeatherForecast/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
+using System.Net.Http;
 using System.Text.Json;
 using WeatherForecast.Models;
 
@@ -16,22 +18,28 @@ namespace WeatherForecast.Controllers
             _httpClient = httpClient;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int daysChosen)
         {
-
-            var httpClient = new HttpClient();
-            httpClient.BaseAddress = new Uri("https://localhost:7082/api/WeatherForecast/");
-            using HttpResponseMessage response = await httpClient.GetAsync(httpClient.BaseAddress);
+            string url = "https://localhost:7082/api/WeatherForecast";
+            if (daysChosen != 0) 
+            {
+                url = $"https://localhost:7082/api/WeatherForecast?daysChosen={daysChosen}";
+            }
+            using HttpResponseMessage response = await _httpClient.GetAsync(url);
             response.EnsureSuccessStatusCode();
             string responseBody = await response.Content.ReadAsStringAsync();
             Console.WriteLine(responseBody);
             Root forecast = JsonSerializer.Deserialize<Root>(responseBody);
+            foreach (var item in forecast.list)
+            {
+                item.main.temp = item.main.temp - 273.15;
+            }
             //Console.WriteLine(forecast.name);
             //Console.WriteLine(forecast.main.temp);
             //Console.WriteLine(forecast.main.feels_like);
-            ViewData["Weather"] = forecast.weather;
-            ViewData["IconUrl"] = $"https://openweathermap.org/img/wn/{forecast.weather.First().icon}.png";
-            return View();
+            ViewData["Weather"] = forecast.list;
+            //ViewData["IconUrl"] = $"https://openweathermap.org/img/wn/{weather.weather.First().icon}.png";
+            return View(forecast);
         }
 
         public IActionResult Privacy()
