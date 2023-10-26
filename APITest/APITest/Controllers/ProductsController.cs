cb4eb2b8ec1dc49dabad6231afbf5cc98025323c
Kingu20 <kiettn12@gmail.com>
2023-10-26 10:07:07 +0200

    Update

--- APITest/APITest/Controllers/ProductsController.cs
+++ APITest/APITest/Controllers/ProductsController.cs
@@ -31,11 +31,67 @@ namespace APITest.Controllers
 		public async Task<ActionResult> GetProduct(int id)
 		{
 			var product = await _shopContext.Products.FindAsync(id);
-			if(product == null)
+			if (product == null)
 			{
 				return NotFound();
 			}
 			return Ok(product);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<Product>> PostProduct(Product product)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest();
+			}
+			_shopContext.Products.Add(product);
+			await _shopContext.SaveChangesAsync();
+
+			return CreatedAtAction(
+				"GetProduct",
+				new {id = product.Id},
+				product);
+		}
+
+		[HttpPut]
+		public async Task<ActionResult> PutProduct(int id, Product product)
+		{
+			if (id != product.Id)
+			{
+				return BadRequest(); 
+			}
+
+			_shopContext.Entry(product).State = EntityState.Modified;
+			try
+			{
+				await _shopContext.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if(!_shopContext.Products.Any(p => p.Id == id)) 
+				{
+					return NotFound(); 
+				}
+				else
+				{
+					throw; 
+				}
+			}
+			return Ok(product);
+		}
+
+		[HttpDelete("{id")]
+		public async Task<ActionResult<Product>> DeleteProduct(int id)
+		{
+			var product = await _shopContext.Products.FindAsync(id);
+			if (product == null)
+			{
+				return NotFound(); 
+			}
+			_shopContext.Products.Remove(product);
+			await _shopContext.SaveChangesAsync();
+			return product;
 		}
 	}
 }
---
+++ Test/Test/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        private const string URL = "https://docs.microsoft.com/en-us/dotnet/csharp";
+        static void Main(string[] args)
+        {
+            DoSynchronousWork();
+            var someTask = DoSomethingAsync();
+            DoSynchronousWorkAfterAwait();
+            someTask.Wait();
+            Console.Write("\nPress any key to terminate...");
+            Console.ReadLine(); 
+        }
+
+        public static void DoSynchronousWork()
+        {
+            Console.WriteLine("Doing some work synchronously");
+        }
+
+        static async Task DoSomethingAsync()
+        {
+            Console.WriteLine("Async task has started...");
+            await GetStringAsync();
+        }
+
+        static async Task GetStringAsync()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                Console.WriteLine("Awaiting the result of GetStringAsync of HTTP client...");
+                string result = await httpClient.GetStringAsync(URL);
+                Console.WriteLine("The awaited task has completed. Let's get the content length..");
+                Console.WriteLine("The length of http Get for {URL}");
+                Console.WriteLine($"{result.Length} character");
+
+            }
+        }
+
+        static void DoSynchronousWorkAfterAwait()
+        {
+            Console.WriteLine("While waiting for the async task to finish, we can do some unrelated...");
+            for (var i = 0; i <= 5; i++)
+            {
+                for (var j = 0; j <= 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
