022f3b5c365c070075dbf4b25f14e1cdc69817f3
Kiet Nguyen <kietn72@gmail.com>
2023-11-16 11:55:39 +0100

    update

---
+++ Cryptography/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Cryptography
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Cryptography cryptography = new Cryptography();
+
+			Console.WriteLine(cryptography.GetHashValue("skattekronen"));
+			Console.WriteLine(cryptography.GetHashValue("tanketorsken"));
+			Console.WriteLine(cryptography.GetHashValue("tanketorsken"));
+			Console.ReadLine(); 
+		}
+	}
+
+	public class Cryptography
+	{
+		public int GetHashValue(string password)
+		{
+			int result = 0;
+			int multiplier = 1;
+			foreach (char ch in password)
+			{
+				result += ch.GetHashCode() * multiplier;
+				multiplier++;
+			}
+			return result % 100;
+		}
+	}
+}
